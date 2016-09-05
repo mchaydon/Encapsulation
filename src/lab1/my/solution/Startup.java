be69a1e7db06feabcad912abcee8ea907a8fc78d
Mike <Mike@10.30.19.233>
2016-09-05 01:55:36 -0500

    Setup a way for Employee to provide info

--- src/lab1/my/solution/Startup.java
+++ src/lab1/my/solution/Startup.java
@@ -1,27 +1,27 @@
 package lab1.my.solution;
 
+import java.util.Scanner;
 
-/**
- * In this lab your challenge is to fix the code in both classes to use
- * proper encapsulation and the other best practices as explained by 
- * your instructor.
- *
- * @author      Jim Lombardo, WCTC Instructor
- * @version     1.01
- */
 public class Startup {
     public static void main(String[] args) {
-        /*
-            Are we violating SRP (Single Responsibility Principle)?
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your first name: ");
+        String fname = input.nextLine();
+        System.out.print("Enter your last name: ");
+        String lname = input.nextLine();
+        System.out.print("Enter your SSN: ");
+        String ssn = input.nextLine();
         
-            HINT: think about what we are trying to do ... hire an employee.
-            Whose job is it to do this? The current class? Or, a new class?
-            Should we do the work here or delegate to the new class? You
-            may need to create a new class that talks to Employee. This does
-            not have to be a GUI class.
-        */
+        
+        
         Manager hr = new Manager();
         
+        
+        
+        hr.hireEmployee(fname, lname, ssn);
+        System.out.printf("\n\n%s %s Report\n", fname, lname);
+        hr.orientEmployee();
+        hr.checkNewHireStatus();
     }
     
     
